efca1eadf1f6aaa52dfdc515168ee1729b66074c
antoniosos <67897276+antoniosos@users.noreply.github.com>
2023-06-20 16:00:41 +0200

    zapomnel jsem to tad do phpka lmao

--- HomePage/testPage.php
+++ HomePage/testPage.php
@@ -1,3 +1,4 @@
+<html !DOCTYPE>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -15,16 +16,29 @@
             <a href="HomePage\testPage.php">My book list</a>
         </h1>
         <nav class="navbar">
-            <a href="#">About us</a>
-            <a href="/AddBook/addBook.php">Add Book</a>
-            <span class="session-variable">
-                <?php
-                    session_start();
-                    if(isset($_SESSION['email'])) {
-                        echo $_SESSION['email'];
-                    }
-                ?>
-            </span>
+            <div class="navbar-content">
+                <a href="/AddBook/addBook.php">Add Book</a>
+                <a href="Navbar\AboutUs.html">About Us</a>
+                <span class="session-variable">
+                    <?php
+                        session_start();
+                        if(isset($_SESSION['email'])) {
+                            echo $_SESSION['email'];
+                        }
+                    ?>
+                </span>
+            </div>
+            <select name="navigation" id="mobile-navigation">
+                <option value="add book">
+                    <a href="/AddBook/addBook.php">Add Book</a>
+                </option>
+                <option value="adfasdfas">
+                    <a href="Navbar\AboutUs.html">About Us</a>
+                </option>
+                
+                
+            </select>
+
         </nav>
     </header>
     
